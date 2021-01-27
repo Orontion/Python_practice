2818f678d95b96c9e7ef5ea555f43cb3219f5a6f
Bosman <orontion@gmail.com>
2021-01-27 03:25:06 +0300

    Added simple function, corrected code style to match Python naming conventions

--- Matrix_multiplication/main.py
+++ Matrix_multiplication/main.py
@@ -1,73 +1,54 @@
-DimR_1 = int(input("Enter rows matrix 1: "))
-DimC_1 = int(input("Enter columns matrix 1: "))
+import re
 
-DimR_2 = int(input("Enter rows matrix 2: "))
 
-CorrectInput = False
-while not CorrectInput:
-    DimC_2 = int(input("Enter columns matrix 2: "))
+def get_valid_input(input_pattern, prompt_text, split_char):
+    correct_input = False
+    while not correct_input:
+        input_string = input(prompt_text)
 
-    if DimC_2 == DimR_1:
-        CorrectInput = True
-    else:
-        print("Incorrect input! Columns count of matrix 2 should be equal to rows count of matrix 1!")
+        print(input_string)
 
-print("Please enter matrix 1 row by row: \r\n")
-Matrix1 = []
-for Rows in range(DimR_1):
+        correct_input = re.match(input_pattern, input_string)
 
-    CorrectInput = False
-    while not CorrectInput:
-        TempList = list(input("").split(','))
+    return list(map(lambda x: int(x), input_string.split(split_char)))
 
-        if len(TempList) == DimC_1:
-            CorrectInput = True
-        else:
-            print("Incorrect input! Columns count in matrix 1 should be equal to {}".format(DimC_1))
 
-    TempRow = []
-    for i in range(len(TempList)):
-        TempRow.append(int(TempList[i]))
+m1_dimensions_input_pattern = '[1-9],[1-9]'
+m1_rows, m1_columns = get_valid_input(m1_dimensions_input_pattern, "Please enter matrix 1 dimensions: ", ',')
 
-    Matrix1.append(TempRow)
+m2_dimensions_input_pattern = f'{m1_columns},[1-9]'
+m2_rows, m2_columns = get_valid_input(m2_dimensions_input_pattern, "Please enter matrix 2 dimensions: ", ',')
 
-for i in range(len(Matrix1)):
-    print(Matrix1[i])
+m1_rows_input_pattern = f"^\\d+(?:,\\d+){{{m1_columns-1}}}$"
+matrix_1 = []
+for row in range(m1_rows):
+    matrix_1.append(get_valid_input(m1_rows_input_pattern, f"Please enter {row+1} row of matrix 1: ", ','))
 
-print("Please enter matrix 2 row by row: \r\n")
-Matrix2 = []
-for Rows in range(DimR_2):
+print("\r\n\r\nMatrix 1:")
+for i in range(len(matrix_1)):
+    print(matrix_1[i])
 
-    CorrectInput = False
-    while not CorrectInput:
-        TempList = list(input("").split(','))
+m2_rows_input_pattern = f"^\\d+(?:,\\d+){{{m2_columns-1}}}$"
+matrix_2 = []
+for row in range(m2_rows):
+    matrix_2.append(get_valid_input(m2_rows_input_pattern, f"Please enter {row+1} row of matrix 2: ", ','))
 
-        if len(TempList) == DimC_2:
-            CorrectInput = True
-        else:
-            print("Incorrect input! Columns count in matrix 2 should be equal to {}".format(DimC_2))
+print("\r\n\r\nMatrix 2:")
+for i in range(len(matrix_2)):
+    print(matrix_2[i])
 
-    TempRow = []
-    for i in range(len(TempList)):
-        TempRow.append(int(TempList[i]))
+matrix_result = []
+for row in range(m1_rows):
+    row_result = []
+    for column in range(m2_columns):
+        result = 0
+        for current_item in range(m1_columns):
+            result += (matrix_1[row][current_item] * matrix_2[current_item][column])
 
-    Matrix2.append(TempRow)
+        row_result.append(result)
 
-for i in range(len(Matrix2)):
-    print(Matrix2[i])
+    matrix_result.append(row_result)
 
-MatrixResult = []
-
-for Rows in range(DimR_1):
-    RowResult = []
-    for Columns in range (DimC_2):
-        Result = 0
-        for ProcItem in range(DimC_1):
-            Result+=(Matrix1[Rows][ProcItem]*Matrix2[ProcItem][Columns])
-
-        RowResult.append(Result)
-
-    MatrixResult.append(RowResult)
-
-for i in range(len(MatrixResult)):
-    print(MatrixResult[i])
+print("\r\n\r\nResult:")
+for i in range(len(matrix_result)):
+    print(matrix_result[i])
